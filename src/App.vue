eb6590f359f4a9de9265ac22c819a52139581764
jcsamoyed <aruby5223@gmail.com>
2022-10-30 00:23:08 +0800

    build: 引入 reset.css

--- src/App.vue
+++ src/App.vue
@@ -7,12 +7,12 @@
 </template>
 
 <style lang="scss">
+@import '@/assets/scss/reset.scss';
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
 }
 
 nav {
