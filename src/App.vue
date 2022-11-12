94e4a3111f46feff8cc072969c7401b9a4c6639f
jcsamoyed <aruby5223@gmail.com>
2022-11-12 15:03:05 +0800

    feat: 新增 footer 元件

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
   <TheNavbar />
   <router-view />
+  <TheFooter />
 </template>
 
 <script>
 import TheNavbar from '@/components/TheNavbar.vue';
+import TheFooter from '@/components/TheFooter.vue';
 
 export default {
   components: {
     TheNavbar,
+    TheFooter,
   },
 };
 </script>
---
+++ src/components/TheFooter.vue
@@ -0,0 +1,32 @@
+<template>
+  <footer>
+    <p>Copyright © 2022 HexSchool.All rights reserved.</p>
+  </footer>
+</template>
+
+<style lang="scss" scoped>
+footer {
+  background: #de611e;
+  padding: 34px;
+}
+p {
+  color: #fff;
+  font-size: 16px;
+  text-align: center;
+}
+
+@media screen and (max-width: 768px) {
+  footer {
+    padding: 10px;
+  }
+  p {
+    font-size: 12px;
+  }
+}
+
+@media screen and (max-width: 640px) {
+  footer {
+    padding: 20px;
+  }
+}
+</style>
